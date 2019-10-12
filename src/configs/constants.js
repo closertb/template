77807d6702f92eed6c0e2ae747676e14cf9c9598
tangbo <bo.tang3@56qq.com>
2019-10-12 16:27:37 +0800

    feat: edit config

--- src/configs/constants.js
+++ src/configs/constants.js
@@ -1,6 +1,5 @@
-export const SITE_NAME = '一个dva网站模板';
+export const SITE_NAME = '一个React + Dva-Core SSr模板';
 export const SITE_ADDRESS = 'closer.site';
-export const APP_CLIENT_ID = 336;
 
 // 页面路径
 export const Paths = {
